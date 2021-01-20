86c59d3c8cc6113d251d7ce63cbfeee5288af9e7
marcosradix <marcosradix@gmail.com>
2021-01-20 19:28:13 -0300

    terminado loan service

--- src/test/java/br/com/workmade/libraryApi/services/impl/LoanServiceTest.java
+++ src/test/java/br/com/workmade/libraryApi/services/impl/LoanServiceTest.java
@@ -2,12 +2,14 @@ package br.com.workmade.libraryApi.services.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,6 +22,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import br.com.workmade.libraryApi.exception.AlreadyLoanedBookFoundException;
 import br.com.workmade.libraryApi.models.Book;
 import br.com.workmade.libraryApi.models.Loan;
+import br.com.workmade.libraryApi.repository.BookRepositoryTest;
 import br.com.workmade.libraryApi.repository.LoanRepository;
 import br.com.workmade.libraryApi.services.BookService;
 import br.com.workmade.libraryApi.services.LoanService;
@@ -81,8 +84,52 @@ public class LoanServiceTest {
 	}
 	
 	
+    @Test
+    @DisplayName("Deve obter informações de um empréstimo por ID")
+    public void getLoanDetailsTest(){
+    	Long id = 1L;
+    	Loan loan = createLoan();
+    	loan.setId(id);
+    	
+    	 when(loanRepository.findById(id)).thenReturn(Optional.of(loan));
+    	 
+    	 Optional<Loan> loanFound = loanService.findById(id);
+    	 
+    	 assertThat(loanFound.isPresent()).isTrue();
+    	 assertThat(loanFound.get().getId()).isEqualTo(id);
+    	 assertThat(loanFound.get().getCustomer()).isEqualTo(loan.getCustomer());
+    	 assertThat(loanFound.get().getBook()).isEqualTo(loan.getBook());
+    	 assertThat(loanFound.get().getLoanDate()).isEqualTo(loan.getLoanDate());
+    	 
+    	 verify(loanRepository).findById(id);
+    	
+    }
 	
-	
+    
+    @Test
+    @DisplayName("Deve atualizar um empréstimo")
+    public void updateLoanTest(){
+       	Long id = 1L;
+    	
+		Loan loan = createLoan();
+		loan.setReturned(true);
+		loan.setId(id);
+				
+		when(loanRepository.save(loan)).thenReturn(loan);
+		
+		Loan saved = loanService.save(loan);
+		
+		assertNotNull(saved);
+		verify(loanRepository).save(loan);
+		
+    	
+    }
+    
+    private Loan createLoan(){
+		Book book = Book.builder().title("Aventuras").author("Fulano").isbn("123").build();
+    	Loan loan = Loan.builder().book(book).customer("Fulano").loanDate(LocalDate.now()).build();
+        return loan;
+    }
 	
 	private Book createBook() {
 		return Book.builder().id(11L).title("Meu Livro").author("Author").isbn("1213212").build();
